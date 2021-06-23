51c4e0b589a36694423485d806ae3019760d5d25
b1ack917 <397615103@qq.com>
2021-06-23 20:09:30 +0800

    [Add] FileUpload

--- Src/js/index.js
+++ Src/js/index.js
@@ -5,26 +5,26 @@ function showLoginBox() {
   let body = document.querySelector('body')
   let loginFrame = document.createElement('div')
   loginFrame.innerHTML =
-    '<form>\
-      <div class="login-header">\
-        <img src="icons/tabler-icon-activity.png">\
-        <p>Login</p>\
-      </div>\
-      <div class="login-input-box">\
-        <i class="fa fa-user" aria-hidden="true" style="width: 20px;"></i>\
-        <input type="text" placeholder="账户">\
-      </div>\
-      <div class="login-input-box">\
-        <i class="fa fa-lock" aria-hidden="true" style="width: 20px;"></i>\
-        <input type="password" placeholder="密码">\
-      </div>\
-    </form>\
-    <div class="login-button-box">\
-      <button>登录</button>\
+    '<form id="loginArgs" method="POST" action="./login">\
+    <div class="login-header">\
+      <img src="icons/tabler-icon-activity.png">\
+      <p>Login</p>\
     </div>\
-    <div class="back-arrow">\
-      <i class="fa fa-arrow-left" aria-hidden="true" onclick="hideLoginBox()"></i>\
-    </div>'
+    <div class="login-input-box">\
+      <i class="fa fa-user" aria-hidden="true" style="width: 20px;"></i>\
+      <input type="text" placeholder="账户" name="user">\
+    </div>\
+    <div class="login-input-box">\
+      <i class="fa fa-lock" aria-hidden="true" style="width: 20px;"></i>\
+      <input type="password" placeholder="密码" name="password">\
+    </div>\
+  </form>\
+  <div class="login-button-box">\
+    <button onclick="postAccount()">登录</button>\
+  </div>\
+  <div class="back-arrow">\
+    <i class="fa fa-arrow-left" aria-hidden="true" onclick="hideLoginBox()"></i>\
+  </div>'
   loginFrame.id = 'loginFrame'
   loginFrame.className = 'animate__animated animate__backInRight'
   body.appendChild(loginFrame)
@@ -38,17 +38,22 @@ function hideLoginBox() {
   let body = document.querySelector('body')
   let main = document.createElement('div')
   main.innerHTML =
-    '<h1 style="text-align: center; color: white; height: 80px; ">上传与分享您的图片</h1>\
+    '<div id="main">\
+    <h1 style="text-align: center; color: white; height: 80px; ">上传与分享您的图片</h1>\
     <h3 style="text-align: center; color: white; height: 80px;">\
       任意拖放图片到这里, 即开始上传你的图片. <br>\
       或者点击下面的图标以开始批量上传你的图片.\
     </h3>\
-    <div class="upload">\
+    <form action="./upload" method="post" style="display: none;" id="fileUploadForm">\
+      <input type="file" name="images" id="fileInput" onchange="postFile()">\
+    </form>\
+    <div class="upload" id="uploadBtn" onclick="selectFile()">\
       <a href="#">\
         <i class="fa fa-cloud-upload" aria-hidden="true"></i>\
         开始上传\
       </a>\
-    </div>'
+    </div>\
+  </div>'
   main.id = 'main'
   body.appendChild(main)
   moveElementToCenter('main')
@@ -57,4 +62,18 @@ function hideLoginBox() {
 function postAccount() {
   let args = document.getElementById('loginArgs')
   args.submit()
+}
+
+function selectFile() {
+  document.getElementById('fileInput').click()
+}
+
+function postFile() {
+  let fileInput = document.getElementById('fileInput')
+  let filename = fileInput.value
+  var index = filename.lastIndexOf('.')
+  var suffix = filename.substr(index + 1)
+  if (suffix in { jpg: true, jpeg: true, png: true })
+    document.getElementById('fileUploadForm').submit()
+  else return
 }
